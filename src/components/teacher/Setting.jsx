4e110902439687e40b534841d7b8d538a87d9d77
MohammadJavad Asgari <mdjdasgari@gmail.com>
2025-10-01 18:31:42 +0330

    prev

--- src/components/teacher/Setting.jsx
+++ src/components/teacher/Setting.jsx
@@ -1,5 +1,13 @@
+import { useEffect } from "react";
 import styles from "../../styles/teacher/setting.module.css"
 const Setting = ({ setMenuPage }) => {
+
+    const handleBackButton = () => {
+        setMenuPage(0)
+    }
+    useEffect(() => {
+        window.addEventListener("popstate", handleBackButton);
+    }, [])
     return (
         <div className={styles.container}>
             <div className={styles.header}>
